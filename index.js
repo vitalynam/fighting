364ccbe9de7a88ca090099d04e6e34d062ada0f5
Namiko01 <vitaliknam01@gmail.com>
2025-08-25 04:13:28 +0300

    feat: others

--- index.js
+++ index.js
@@ -7,18 +7,17 @@ let player = {
 }
 const submitButton = document.querySelector('.registration__submit');
 const chose = document.querySelectorAll('.person-logo');
+const changeAvatar = document.querySelector('.edit-avatar');
+const enemyBlog = document.querySelector('.enemy-wrap');
 
-// if(localStorage.getItem()){
-//     console.log(localStorage.getItem('name'))
-// }
-
-for (const element of chose) {
-    element.addEventListener('click', ()=>{
-        player.ava = `images/persons/${element.id}-logo.png`;
-        player.pers = `images/persons/${element.id}.gif`;
-
-        setImage(player.ava, player.pers)
-    })
+if(localStorage.getItem('person')){
+    let r = localStorage.getItem('person');
+    player = JSON.parse(r);
+    document.querySelector('.name').innerHTML = player.name;
+    document.querySelector('.wins').innerHTML = player.wins;
+    document.querySelector('.lose').innerHTML = player.lose;
+    document.querySelector('.person__info_logo').innerHTML = `<img src="${player.ava}" alt="sub-zero">`;
+    document.querySelector('.fight__player').innerHTML = `<img src="${player.pers}" alt="sub">`;
 }
 
 function setName (name){
@@ -30,12 +29,32 @@ function setImage(urlLogo , urlPerson){
     document.querySelector('.fight__player').innerHTML = `<img src="${urlPerson}" alt="sub">`;
     localStorage.setItem('person', JSON.stringify(player));
 }
+function setWins(counter){
+    document.querySelector('.wins').innerHTML = `${counter}`;
 
+    localStorage.setItem('person', JSON.stringify(player));
+}
+function setLose(counter){
+    document.querySelector('.lose').innerHTML = `${counter}`;
 
-
+    localStorage.setItem('person', JSON.stringify(player));
+}
 
 submitButton.addEventListener('click', ()=>{
     player.name = document.querySelector('.registrtion__name').value;
     setName(player.name)
 })
 
+for (const element of chose) {
+    element.addEventListener('click', ()=>{
+        player.ava = `images/persons/${element.id}-logo.png`;
+        player.pers = `images/persons/${element.id}.gif`;
+        setImage(player.ava, player.pers);
+        document.querySelector('.person__info_check').classList.remove('active');
+        enemyBlog.classList.remove('active');
+    })
+}
+changeAvatar.addEventListener('click', ()=>{
+    document.querySelector('.person__info_check').classList.toggle('active');
+    enemyBlog.classList.toggle('active');
+})
